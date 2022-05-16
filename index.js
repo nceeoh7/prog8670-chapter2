6ab544f4412fec5d6a200d9b1a36f599db176987
nceeoh7 <51413645+nceeoh7@users.noreply.github.com>
2022-05-16 14:44:38 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+  console.log("App listening on port 3000");
+});
+
+app.get("/", (req, res) => {
+  res.json({
+    name: "Welcome to Express",
+  });
+});
---
+++ index2.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.listen(3000, () => {
+  console.log("App listening on port 3000");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "about.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "contact.html"));
+});
